7f926706341794d496afbf275c66c9d0bb04a2f3
ruwaa33 <71657974+ruwaa33@users.noreply.github.com>
2020-12-25 23:40:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <h1>Pass your AWS exam with our proxy exam tool. We will remotely do the exam for you</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    <h3>Pay after you pass🎉</h3>
 
     <style jsx>{`
       :global(html,body) {
